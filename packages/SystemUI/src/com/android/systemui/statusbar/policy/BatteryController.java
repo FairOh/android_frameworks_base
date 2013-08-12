0b7a507784d7ad88c088b5472e41c6faefe2840d
rusty <slayjay78@gmail.com>
2013-08-11 18:19:59 -0700

    Bring circle battery back

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
@@ -42,7 +42,16 @@ public class BatteryController extends BroadcastReceiver {
 
     private static final int BATTERY_STYLE_NORMAL         = 0;
     private static final int BATTERY_STYLE_PERCENT        = 1;
-    private static final int BATTERY_STYLE_GONE           = 2;
+    /***
+     * BATTERY_STYLE_CIRCLE* cannot be handled in this controller, since we cannot get views from
+     * statusbar here. Yet it is listed for completion and not to confuse at future updates
+     * See CircleBattery.java for more info
+     *
+     * set to public to be reused by CircleBattery
+     */
+    public  static final int BATTERY_STYLE_CIRCLE         = 2;
+    public  static final int BATTERY_STYLE_CIRCLE_PERCENT = 3; 
+    private static final int BATTERY_STYLE_GONE           = 4;
     private static final int BATTERY_ICON_STYLE_NORMAL      = R.drawable.stat_sys_battery;
     private static final int BATTERY_ICON_STYLE_CHARGE      = R.drawable.stat_sys_battery_charge;
     private static final int BATTERY_TEXT_STYLE_NORMAL  = R.string.status_bar_settings_battery_meter_format;
